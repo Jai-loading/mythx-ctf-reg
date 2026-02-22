16f6aa903b18652ff987808f1eeb4e1b0348963a
Jai-loading <jai99.loading@gmail.com>
2026-02-23 00:26:59 +0530

    Production: remove dotenv and use ecosystem env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,18 @@
 // ecosystem.config.js
-require('dotenv').config()
 
 module.exports = {
     apps: [
         {
             name: "mythx",
             script: "server.js",
-            cwd: "/var/www/mythx/app", // important for EC2
+            cwd: "/var/www/mythx/app",
             env: {
                 NODE_ENV: "production",
-                AWS_REGION: process.env.AWS_REGION,
-                DYNAMODB_TABLE_NAME: process.env.DYNAMODB_TABLE_NAME,
-                AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME,
-                NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH
+                PORT: 3000,
+                AWS_REGION: "ap-south-1",
+                DYNAMODB_TABLE_NAME: "MythX_CTF_Players",
+                AWS_S3_BUCKET_NAME: "mythx-ctf-registrants",
+                NEXT_PUBLIC_BASE_PATH: "/ctf2026"
             }
         }
     ]
